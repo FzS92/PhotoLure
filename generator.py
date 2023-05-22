046057d7fdd2855337c3a30de65b4709904be229
Farzad Salajegheh <salajegheh@outlook.com>
2023-05-22 15:51:16 -0400

    Download the result

--- generator.py
+++ generator.py
@@ -4,16 +4,21 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
+import wget
 
 # Parameters
 img = 'data/banana.png'
 mask = 'data/mask_banana.png'
+out = 'data/out_banana.png'
+
+
 # prompt = 'A sunlit indoor lounge area with a pool containing a dog'
 # prompt = 'A sunlit indoor lounge area with a pool'
 # prompt = 'two young men on his luxury huge private yacht, sailing in the bahamas with palm trees in the background and hardwood deck on the yacht, cinematic, nature, hyperrealistic, 8 k'
 # prompt = 'At the background scary lightning black and white, Real, nature, ultra detailed, 8k'
 # prompt = 'we are in a luxury huge private yacht, sailing in the bahamas with palm trees in the background and hardwood deck on the yacht, cinematic, nature, hyperrealistic, 8 k'
-prompt = 'we are in a luxury huge private yacht on water and sunshine in the background'
+# prompt = 'we are in a luxury huge private yacht on water and sunshine in the background'
+prompt = 'Banana on the table and in the background scary lightning black and white, Real, nature, ultra detailed, 8k'
 
 
 # setting up openai
@@ -28,8 +33,6 @@ response = openai.Image.create_edit(
     n=1,
     size="512x512"
 )
-print(response)
 image_url = response['data'][0]['url']
-
-
+filename = wget.download(image_url, out=out)
 print(image_url)
