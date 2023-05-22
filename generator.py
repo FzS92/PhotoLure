9e9ca994de2509e03dc980d73d3b73487c06acd5
Farzad Salajegheh <salajegheh@outlook.com>
2023-05-22 08:22:59 -0400

    add background

--- generator.py
+++ generator.py
@@ -6,9 +6,11 @@ import torchvision.transforms as transforms
 from torchvision.utils import save_image
 
 # Parameters
-img = 'data/image.png'
-mask = 'data/mask2.png'
-prompt = 'A sunlit indoor lounge area with a pool containing a dog'
+img = 'data/IMG-20220730-WA0037.png'
+mask = 'data/mask_IMG-20220730-WA0037.png'
+# prompt = 'A sunlit indoor lounge area with a pool containing a dog'
+# prompt = 'two young men on his luxury huge private yacht, sailing in the bahamas with palm trees in the background and hardwood deck on the yacht, cinematic, nature, hyperrealistic, 8 k'
+prompt = 'At the background scary lightning black and white, Real, nature, ultra detailed, 8k'
 
 # setting up openai
 openai_key = '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'
@@ -16,14 +18,13 @@ openai.api_key = openai_key
 
 # generate the images
 response = openai.Image.create_edit(
-  image=open(img, "rb"),
-  mask=open(mask, "rb"),
-  prompt=prompt,
-  n=1,
-  size="256x256"
+    image=open(img, "rb"),
+    mask=open(mask, "rb"),
+    prompt=prompt,
+    n=1,
+    size="512x512"
 )
 image_url = response['data'][0]['url']
 
 
 print(image_url)
-
--- segment.py
+++ segment.py
@@ -4,6 +4,18 @@ import matplotlib.pyplot as plt
 import cv2
 from PIL import Image
 from pathlib import Path
+import os
+
+# Parameters
+img_path = "./images/IMG-20220730-WA0037.jpg"
+target_path = "./data"
+target_shape = (256, 256)
+
+# Check if the results directory exists
+if not os.path.exists("results"):
+    # If it doesn't exist, create it
+    os.makedirs("results")
+
 
 # Functions
 def show_mask(mask, ax, random_color=False):
@@ -14,26 +26,28 @@ def show_mask(mask, ax, random_color=False):
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
-    
+
+
 def show_points(coords, labels, ax, marker_size=375):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-    
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green',
+               marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red',
+               marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+
 def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green',
+                 facecolor=(0, 0, 0, 0), lw=2))
+
 
 def pil_save(addr, img):
     pil_image = Image.fromarray(np.uint8(img))
     pil_image.save(addr)
 
-# Parameters
-img_path = "images/man1.jpg"
-target_path = "./data"
-target_shape = (256,256)
 
 # make the target directory
 target_path = Path(target_path)
@@ -41,23 +55,26 @@ target_path.mkdir(exist_ok=True)
 
 
 # load the image
-image = cv2.imread(img_path) 
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # 503 x 406 x 3
-image =  cv2.resize(image,dsize=target_shape, interpolation=cv2.INTER_CUBIC)
+image = cv2.imread(img_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # 503 x 406 x 3
+image = cv2.resize(image, dsize=target_shape, interpolation=cv2.INTER_CUBIC)
 
 
-### The Segment Anything model
+# The Segment Anything model
 model = "vit_h"
 checkpoint = "./sam_vit_h_4b8939.pth"
-device = "cuda"
+device = "cpu"
 
 input_point = np.array([[0, 0]])
 input_label = np.array([1])
 
+print(f"Loading the model on {device}")
 sam = sam_model_registry[model](checkpoint=checkpoint)
 sam.to(device=device)
+print("Done!")
 
 # segment the image
+print("Finding the background at (0,0)")
 predictor = SamPredictor(sam)
 predictor.set_image(image)
 masks, scores, logits = predictor.predict(
@@ -70,25 +87,45 @@ masks, scores, logits = predictor.predict(
 ## Print all masks for the first time to choose the right mask ###
 image_id = Path(img_path).stem
 for i, (mask, score) in enumerate(zip(masks, scores)):
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     plt.imshow(image)
     show_mask(mask, plt.gca())
     show_points(input_point, input_label, plt.gca())
     # mask = torchvision.transforms.Resize((406,503))(mask)
     plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
     plt.axis('off')
-    plt.savefig(f"results/result{i}{image_id}.png")  
+    plt.savefig(f"results/result{i}{image_id}.png")
+
+
+# Increasing backgound space
+# Create a new image with extra pixels
+print("adding additional backgound space")
+x_offset, y_offset = int(256/2), 256
+new_height = target_shape[0] + y_offset
+new_width = target_shape[1] + 2 * x_offset  # x_offset pixels on each side
+new_image = np.ones((new_height, new_width, 3), np.uint8) * 255  # White pixels
+new_image[y_offset:target_shape[0]+y_offset,
+          x_offset:target_shape[1]+x_offset] = image
+
+# Create a new mask with extra pixels
+new_mask = np.ones((new_width, new_height), dtype=bool)
+new_mask[y_offset:target_shape[0]+y_offset,
+         x_offset:target_shape[1]+x_offset] = masks[-1]
 
 
 # choose the right mask ***************
-mask = 1- masks[-1]
+# mask = 1 - masks[-1]
+mask = 1 - new_mask
+
 
 # make the mask RGBA
 new_mask = np.zeros((*mask.shape, 4))
-new_mask[:,:,-1] = mask
-print(image.shape, new_mask.shape)
+
+new_mask[:, :, -1] = mask
+
+# print(image.shape, new_mask.shape)
 
 # save mask and image
 image_id = Path(img_path).stem
-pil_save(str(target_path / f'{image_id}.png'), image)
+pil_save(str(target_path / f'{image_id}.png'), new_image)
 pil_save(str(target_path / f'mask_{image_id}.png'), new_mask*255)
